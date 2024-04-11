8c2bf8c04b1b2995e3220598bca35df3156abf50
Francisco Serafini Giorgi <franciscoserafinigiorgi@gmail.com>
2024-04-11 19:27:00 -0300

    clase1

---
+++ clase-1/funcion1.js
@@ -0,0 +1,7 @@
+export const funcion1 = () =>{
+    return 5 + 5
+}
+
+export const fun2 = () =>{
+    return 10 - 5
+}
---
+++ clase-1/index.js
@@ -0,0 +1,9 @@
+import {funcion1, fun2} from './funcion1.js'
+    
+const array = [1,2,3,4,5,6]
+    
+array.map(e =>{
+    console.log("El numero es: " + e)
+})
+
+console.log("Elresutlado es: ",funcion1())
